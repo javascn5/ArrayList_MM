5065278c0e1b0ffe044def873a1bbfc5842078af
Mates <marczak.mat@gmail.com>
2018-10-21 12:54:39 +0200

    przed dodaniem metody remove

---
+++ src/com/sda/git/CustomList.java
@@ -0,0 +1,60 @@
+package com.sda.git;
+
+// implementacja włąsnej arraylisty
+
+public class CustomList<E> {
+
+    private int size = 0;
+    private final int DEFAULT_CAPACITY = 10; // domyslnie lista ma 10 elementów
+    private Object elements[];
+
+    public CustomList()
+    {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(E e)
+    {
+        if(size == elements.length)
+        {
+            resize();
+        }
+        elements[size++] = e;
+    }
+
+    public void resize()
+    {
+        int newSize = elements.length * 2;
+        Object tmp[] = new Object[newSize];
+
+        for (int i = 0; i < elements.length; i++)
+        {
+            tmp[i] = elements[i];
+
+        }
+
+        elements = tmp;
+    }
+
+
+    public E get(int i)
+    {
+        if(i > size || i < 0)
+        {
+            throw new IndexOutOfBoundsException("Nie ma takiego elementu");
+        }
+         return (E)elements[i];
+    }
+
+    public int size()
+    {
+        return size;
+    }
+
+    public void remove(int index)
+    {
+        //miejsce w tablicy, ktore usuwamy
+    }
+
+
+}
--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -4,5 +4,35 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+            CustomList<String> lista = new CustomList<>();
+
+            lista.add("Jan");
+            lista.add("Maciej");
+            lista.add("Maciej2");
+            lista.add("Maciej3");
+            lista.add("Maciej4");
+            lista.add("Maciej5");
+            lista.add("Maciej6");
+            lista.add("Maciej7");
+            lista.add("Maciej8");
+            lista.add("Maciej9");
+            lista.add("Maciej10");
+            lista.add("Maciej11");
+            lista.add("Maciej12");
+            lista.add("Maciej13");
+            lista.add("Maciej14");
+            lista.add("Maciej15");
+            lista.add("Maciej16");
+
+
+            for(int i=0; i < lista.size(); i++)
+            {
+                System.out.println(lista.get(i));
+            }
+
+
+
+
     }
 }
